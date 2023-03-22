304a31c243e3fb249eac1a768f2fbd0f98a54a26
iambinod <helloitsbinod@gmail.com>
2023-03-22 14:13:19 +0545

    feaT: added side menu

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,4 +1,17 @@
 const SideMenu = () => {
-  return <div>SideMenu</div>;
+  return (
+    <>
+      <div>
+        <div className="flex items-center gap-3">
+          <img
+            src="https://hips.hearstapps.com/hmg-prod/images/gettyimages-594881580.jpg?resize=1200:*"
+            alt="Pedro pascal"
+            className="rounded-full"
+          />
+          <p>Pedro Pascal</p>
+        </div>
+      </div>
+    </>
+  );
 };
 export default SideMenu;
